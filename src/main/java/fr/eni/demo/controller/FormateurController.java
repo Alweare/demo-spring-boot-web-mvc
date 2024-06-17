fecbe45afd758c83279842a7a4ce4a39eaeeeddd
Yanis Renard-dumont <74371878+Alweare@users.noreply.github.com>
2024-06-17 15:36:25 +0200

    Utilisation Annotation Session

--- src/main/java/fr/eni/demo/controller/FormateurController.java
+++ src/main/java/fr/eni/demo/controller/FormateurController.java
@@ -1,28 +1,36 @@
 package fr.eni.demo.controller;
 
+import java.text.Format;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import fr.eni.demo.bll.CoursService;
 import fr.eni.demo.bll.FormateurService;
+import fr.eni.demo.bo.Cours;
 import fr.eni.demo.bo.Formateur;
 
 @Controller
 @RequestMapping("/formateurs")
+@SessionAttributes({"coursSession"})
 public class FormateurController {
 	
 	private FormateurService formateurService;
+	private CoursService coursService;
 	
 	
 	
 	
-	public FormateurController(FormateurService formateurService) {
+	public FormateurController(FormateurService formateurService, CoursService coursService) {
 		this.formateurService = formateurService;
+		this.coursService = coursService;
 	}
 	
 	@GetMapping
@@ -46,7 +54,6 @@ public class FormateurController {
 		
 		System.out.println("emailFormateur = " + emailFormateur);
 		
-		System.out.println(f.getPhoto());
 		
 		return "view-formateur-detail";
 	}
@@ -58,8 +65,34 @@ public class FormateurController {
 		System.out.println("Prénom =" + prenom);
 		System.out.println("Nom = " + nom);
 		System.out.println("Email = " + email);
+		
+		Formateur formateur = new Formateur(nom, prenom, email);
+		
+		this.formateurService.update(formateur);
+		
 		//redirige vers l'adresse responsable de l'affichage des formateurs
 		return "redirect:/formateurs";
+	}
+	@PostMapping("/cours")
+	public String ajouterCours(@RequestParam("email") String email, 
+			@RequestParam("idCours") long idCours) {
+		
+		this.formateurService.updateCoursFormateur(email, idCours);
+		
+		return "redirect:/formateurs/detail?email=" + email;
+		
+	}
+	
+	
+	@ModelAttribute("coursSession")
+	public List<Cours> chargerCoursSession(){
+		
+		List<Cours> listeCours = this.coursService.getCours();
+		System.out.println(listeCours);
+		
+		
+		return listeCours;
+		
 	}
 
 }
